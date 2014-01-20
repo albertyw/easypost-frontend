ac1f884bddbe65233a762e6391a28e4fd81ba300
Albert Wang <albertyw@mit.edu>
2014-01-19 19:57:37 -0800

    Added flask

--- scripts/runserver.sh
+++ scripts/runserver.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 
-# a simple script to remove temp files and restart the server
+# a simple script to run the server
 
-# Check pwd
-if [ ! -f `pwd`/manage.py ]; then
-    echo "You must run this from the root statsonice directory"
-    exit
-fi
-
-
-# Get Settings
-settings='--settings=util.settings_production'
-for i in "$*" ; do
-    if [ "$i" = "--production" ]; then
-        echo 'USING PRODUCTION SETTINGS'
-        settings='--settings=util.settings_development'
-    fi
-done
-
-python manage.py runserver 0.0.0.0:9001 $settings
+python poomailer/serve.py -p 9001
 sleep 2s
 /bin/bash scripts/runserver.sh "$@"
