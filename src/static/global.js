f9b035d5fa7134ed9a1bb05578107c63e687fc9d
Albert Wang <albertyw@mit.edu>
2014-02-22 15:52:10 -0800

    Simplified js

--- src/static/global.js
+++ src/static/global.js
@@ -1,41 +1,17 @@
 function submitAddress(){
   $("#submitButton").hide();
   $("#submitButtonLoading").show();
-  var name = $("#full-name").val();
-  var address_line1 = $("#address-line1").val();
-  var address_line2 = $("#address-line2").val();
-  var city = $("#city").val();
-  var region = $("#region").val();
-  var postal_code = $("#postal-code").val();
-  var country = $("#country").val();
-  var phone = $("#phone").val();
-  var print_custom_1 = $("#print_custom_1").val();
-  var length = $("#length").val();
-  var width = $("#width").val();
-  var height = $("#height").val();
-  var weight = $("#weight").val();
-  var dry_ice = $("#dry-ice").val();
-  var email = $("#email").val();
+  data = {};
+  $("input[type!='submit']").each(function(index, input){
+      console.log(input);
+      data[input.id] = input.value
+  });
+  console.log(data);
+  return false;
   $.ajax({
     type:'POST',
     url:'/submit',
-    data:{
-      name:name,
-      address_line1:address_line1,
-      address_line2:address_line2,
-      city:city,
-      region:region,
-      postal_code:postal_code,
-      country:country,
-      phone:phone,
-      print_custom_1:print_custom_1,
-      length:length,
-      width:width,
-      height:height,
-      weight:weight,
-      dry_ice:dry_ice,
-      email:email
-    },
+    data:data,
     success:function(data){
       if(data['status'] == 'error'){
         showMessage(data['message'], data['status']);
