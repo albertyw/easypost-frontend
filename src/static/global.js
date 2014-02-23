09d8c0568b25555108df1c6ae6719d94975dff59
Albert Wang <albertyw@mit.edu>
2014-02-23 01:13:54 -0800

    Added function to autopopulate form with test data

--- src/static/global.js
+++ src/static/global.js
@@ -1,3 +1,4 @@
+// Event handler for when a person submits the form
 function submitAddress(){
   $("#submitButton").hide();
   $("#submitButtonLoading").show();
@@ -27,6 +28,8 @@ function submitAddress(){
   });
   return false;
 }
+
+// Handler for showing a message from an ajax call to the server
 function showMessage(message, status){
   if(status == 'error'){
     $("#status").addClass('alert-danger');
@@ -39,4 +42,23 @@ function showMessage(message, status){
   $("#status").show('slow');
   $("#submitButton").show();
   $("#submitButtonLoading").hide();
+}
+
+// Populate the form with example data
+function fillExampleData(){
+  $("#name").val('Albert Wang');
+  $("#company").val('Test Company');
+  $("#street1").val('440 Davis Ct');
+  $("#street2").val('Apartment 919');
+  $("#city").val('San Francisco');
+  $("#state").val('California');
+  $("#zip").val('94111');
+  $("#country").val('US');
+  $("#phone").val('617-575-9658');
+  $("#print_custom_1").val('Test custom line 1');
+  $("#length").val('4');
+  $("#width").val('5');
+  $("#height").val('6');
+  $("#weight").val('8');
+  $("#dry_ice_weight").val('2');
 }
