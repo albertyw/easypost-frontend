0b2199e2f8fd66bfcb88ce31d183d0f6bbb17681
Albert Wang <albertyw@mit.edu>
2014-02-23 22:43:52 -0800

    Finished javascript csv upload

--- src/static/global.js
+++ src/static/global.js
@@ -1,3 +1,7 @@
+/**
+ * Form Handlers
+ **/
+
 // Event handler for when the address form is submitted
 function submitAddress(){
   startLoading();
@@ -6,25 +10,7 @@ function submitAddress(){
       data[input.id] = input.value
   });
   data['country'] = $("#country").val();
-  $.ajax({
-    type:'POST',
-    url:'/submit',
-    data:data,
-    success:function(data){
-      if(data['status'] == 'error'){
-        showMessage(data['message'], data['status']);
-      }else if(data['status'] == 'success'){
-        text = 'New label created.  ';
-        text = 'Tracking Code: '+data['message']['tracking_code']+' ';
-        text += '<a href="'+data['message']['label_url']+'">Get Label</a>';
-        showMessage(text, data['status']);
-      }
-    },
-    error:function(){
-      showMessage('Unknown Error');
-    },
-    dataType:'json'
-  });
+  ajaxAddress(data);
   return false;
 }
 
@@ -53,13 +39,63 @@ function readCsvForm(){
   return false;
 }
 
+
+/**
+ * Data processing and pushing
+ **/
+
 function processCsvData(address_contents, shipment_contents){
   //address_contents = 'Customer Org,Ship Name (Attn:),Address 1,Address 2,City,ST,ZIP,Phone,Email@email.com'
   //shipment_contents = '2/12/2014,Shipment #,Customer Org,Length,Width,Height,Weight,Ice Weight,PO#'
-  console.log(address_contents)
-  console.log(shipment_contents)
+  address_contents = address_contents.split(',');
+  shipment_contents = shipment_contents.split(',');
+  data = {};
+  data.name = address_contents[1];
+  data.company = address_contents[0];
+  data.street1 = address_contents[2];
+  data.street2 = address_contents[3];
+  data.city = address_contents[4];
+  data.state = address_contents[5];
+  data.zip = address_contents[6];
+  data.country = 'US'
+  data.phone = address_contents[7];
+  data.print_custom_1 = shipment_contents[8].trim();
+  data.length = shipment_contents[3];
+  data.width = shipment_contents[4];
+  data.height = shipment_contents[5];
+  data.weight = shipment_contents[6];
+  data.dry_ice_weight = shipment_contents[7];
+  ajaxAddress(data);
   stopLoading();
 }
+
+// Function to send data to server using ajax
+function ajaxAddress(data){
+  $.ajax({
+    type:'POST',
+    url:'/submit',
+    data:data,
+    success:function(data){
+      if(data['status'] == 'error'){
+        showMessage(data['message'], data['status']);
+      }else if(data['status'] == 'success'){
+        text = 'New label created.  ';
+        text = 'Tracking Code: '+data['message']['tracking_code']+' ';
+        text += '<a href="'+data['message']['label_url']+'">Get Label</a>';
+        showMessage(text, data['status']);
+      }
+    },
+    error:function(){
+      showMessage('Unknown Error');
+    },
+    dataType:'json'
+  });
+}
+
+
+/**
+ * UI
+ **/
 
 // Handler for showing a message from an ajax call to the server
 function showMessage(message, status){
@@ -87,6 +123,10 @@ function stopLoading(){
   $("#submitButtonLoading").hide();
 }
 
+
+/**
+ * Utils
+ **/
 
 // Populate the form with example data
 function fillExampleData(){
